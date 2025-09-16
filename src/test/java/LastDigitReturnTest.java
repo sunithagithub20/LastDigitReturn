b0be6ce6bb6e7c8bbafe6a2424b88cfb280c18f3
veenus1128 <sharmilaveenus123@gmail.com>
2025-09-16 15:19:54 +0530

    Done

--- src/test/java/LastDigitReturnTest.java
+++ src/test/java/LastDigitReturnTest.java
@@ -1,4 +1,16 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 public class LastDigitReturnTest {
-
+	@Test
+	public void testCase1()
+	{
+		Assertions.assertEquals(6,LastDigitReturn.lastDigitReturn(786));
+	}
+	@Test
+	public void testCase2()
+	{
+		Assertions.assertEquals(6,LastDigitReturn.lastDigitReturn(-786));
+	}
+	
 }
